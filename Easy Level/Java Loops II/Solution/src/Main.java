289035094d40bb70d90af7cd87ab4ba6ade53f79
Ivan Logutov <foreverivan2004@gmail.com>
2022-10-04 04:45:45 +0300

    Adding java loop task

---
+++ Easy Level/Java Loops II/Solution/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int numBefore = a;
+            for (int j = 0; j < n; j++) {
+                System.out.print( ((int)Math.pow(2,j)*b + numBefore) + " ");
+                numBefore += (int) Math.pow(2,j)*b;
+                // System.out.println(numBefore);
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+}
